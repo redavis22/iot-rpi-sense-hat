02bda85b0226dbeb4adca951a4ae2b91c788dfcd
teuteuguy <pub@lalatte.com>
2016-01-21 15:44:36 +0800

    Update

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ var config = require(__dirname + '/config.json');
 // Configure the Pi's ClientId to match the hostname
 config.clientId = os.hostname();
 
-console.log('Init with config:', config);
+console.log('Start of app for:', config.clientId);
+
+
 
